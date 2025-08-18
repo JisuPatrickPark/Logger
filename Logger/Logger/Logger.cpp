826bca2a373185706d10897535d2b77213ed5edc
JSPARK\csz00 <kes00707@gmail.com>
2025-08-18 17:51:43 +0900

    Create project

---
+++ Logger/Logger/Logger.cpp
@@ -0,0 +1,22 @@
+﻿// Logger.cpp : DLL을 위해 내보낸 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Logger.h"
+
+
+// 내보낸 변수의 예제입니다.
+LOGGER_API int nLogger=0;
+
+// 내보낸 함수의 예제입니다.
+LOGGER_API int fnLogger(void)
+{
+    return 0;
+}
+
+// 내보낸 클래스의 생성자입니다.
+CLogger::CLogger()
+{
+    return;
+}
