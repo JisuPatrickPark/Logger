5b0f79bba06b37113d8d96adce0e3c354c0373ef
JisuPatrickPark <gch00707@gmail.com>
2025-09-03 13:07:36 +0900

    chore: update .gitignore and comment out unused code
    Add .gitignore
    Edit unused code

--- Logger/TestLogger/Test.cpp
+++ Logger/TestLogger/Test.cpp
@@ -5,11 +5,11 @@ using namespace MyLib;
 
 int main() 
 {
-    MyLib::Logger::Info("0x\x41\x27 쁘로토콜");
-    MyLib::Logger::Debug("벌\r레");
-    MyLib::Logger::Warn("월\n월");
-    MyLib::Logger::Error("러\t러\t럴");
-    MyLib::Logger::Critical("\"Critical\" Log");
+    //MyLib::Logger::Info("0x\x41\x27 쁘로토콜");
+    //MyLib::Logger::Debug("벌\r레");
+    //MyLib::Logger::Warn("월\n월");
+    //MyLib::Logger::Error("러\t러\t럴");
+    //MyLib::Logger::Critical("\"Critical\" Log");
 
     //MyLib::Logger::CreateModuleLogger("SDK", "log\\SDK.log", false);
     //auto sdkLogger = MyLib::Logger::GetLogger("SDK");
@@ -28,8 +28,8 @@ int main()
     //}
 
     // MyLib::Logger::SetLevel(spdlog::level::warn);    spdlog 외부라이브러리 포함 안하려면 enum 따로 써야되게따
-    MyLib::Logger::Info("Not Print");
-    MyLib::Logger::Warn("Print");
+    //MyLib::Logger::Info("Not Print");
+    //MyLib::Logger::Warn("Print");
 
     return 0;
 }
